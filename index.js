6046952efe583880102b8f9cb1374e66d53684f3
Brian Tu <feihcsim824@gmail.com>
2017-10-24 21:52:26 -0400

    return truffle contract object and add to window if not already added

--- index.js
+++ index.js
@@ -1,113 +1,61 @@
 const path = require('path')
-const flatten = require('./flatten')
+    , flatten = require('./flatten')
     , truffleCompile = require('truffle-compile')
-    // , solc = require('solc')
-
-const loaderUtils = require('loader-utils')
+    , loaderUtils = require('loader-utils')
 
 module.exports = function(contents){
-  
+
   this.cacheable && this.cacheable()
   const options = loaderUtils.getOptions(this)
-  
+
   /**
-   * to avoid having to save contract artifacts into their
-   * own js fileswhenever a .sol dependency tree exists,
-   * just reduce each tree down to its own single, concatenated file
-   */
+  * to avoid having to save contract artifacts into their
+  * own js fileswhenever a .sol dependency tree exists,
+  * just reduce each tree down to its own single, concatenated file
+  */
   const file = this.resourcePath
   const flattenedSolidityFile = flatten({
     ...options,
     file
   })
 
+  const contractFileName = path.basename(this.resourcePath)
+  const contractName = contractFileName.charAt(0).toUpperCase() + contractFileName.slice(1, contractFileName.length - 4)
 
-  const callback = this.async()
+  const compilationFinished = this.async()
   const truffleCompileOptions = options
-  
+
   truffleCompile([flattenedSolidityFile], truffleCompileOptions, (err, artifact) => {
-    if(err) return callback(err)
-    const resultStr = `'${
+
+    if(err) return compilationFinished(err)
+    const artifactString = `\`${
       JSON.stringify(artifact)
         .replace(/\\"/gi, '\\\\"')
         .replace(/(\\r\\n|\\n|\\r)/gm,'')
-    }'`
-    
-    callback(err, `
+    }\``
+
+    compilationFinished(err, `
+      console.log(String.raw${artifactString})
+      const truffleContract = require('truffle-contract')
       console.log('how many')
-      window.contractObj = window.contractObj || (_ => {
-        console.log('times')
-        var contract = require('truffle-contract')
-        var contractObjs = JSON.parse(${resultStr})
-        var contractObj = Object.values(contractObjs).map(co => contract(co))[0]
-        console.log(contractObj)
-        return contractObj
-      })()
-      console.log('same', contractObj)
-      module.exports = contractObj
+      const contractName = '${contractName}'
+      if(!window[contractName]){
+        const artifacts = JSON.parse(${artifactString})
+        const artifact = artifacts['${contractName}']
+        window[contractName] = truffleContract(artifact)
+      }
+
+      // window.contractObj = window.contractObj || (_ => {
+      //   console.log('times')
+      //   const contractObj = Object.values(contractObjs).map(co => contract(co))[0]
+      //   console.log(contractObjs)
+      //   // console.log(contractObj)
+      //   return contractObj
+      // })()
+      // console.log('same', contractObj)
+      module.exports = window[contractName]
     `)
+
   })
 
-  
-
 }
-
-
-
-// console.log('how mny times')
-
-// var contract = require('truffle-contract')
-//   , truffleCompile = require('truffle-compile')
-//   , solc = require('solc')
-  
-  
-// module.exports = function (solidityFile) {
-  
-//   // console.log('file', solidityFile)
-
-//   this.cacheable && this.cacheable()
-  
-//   var callback = this.async()
-//   var callback2 = (ret1, result, ret3) => {
-//     console.log('reza', result)
-//     // var contractObj = contract(result)
-//     // const util = require('util')
-//     // console.log(util.inspect(result, false, null))
-//     var resultStr = JSON.stringify(result);
-//     // console.log('normale', result)
-//     // console.log('str v', resultStr)
-//     callback(null, `
-//       console.log('how many')
-//       window.contractObj = window.contractObj || (_ => {
-//         console.log('times')
-//         var contract = require('truffle-contract')
-//         var contractObjs = JSON.parse('${resultStr}'.replace(/(\\r\\n|\\n|\\r)/gm,""))
-//         var contractObj = Object.values(contractObjs).map(co => contract(co))[0]
-//         return contractObj
-//       })()
-//       console.log('same', contractObj)
-//       module.exports = contractObj
-//     `)
-//   }
-  
-//   // console.log('just tell me', `${__dirname}/contracts/`)
-//   // console.log('resolver', require('../../truffle'))
-//   // console.log(solc.optimizer)
-//   // truffleCompile.([solidityFile], {
-//   truffleCompile.necessary({
-//     working_directory: './niqua',
-//     contracts_directory: './contracts',
-//     contracts_build_directory: './built-contracts',
-//     // resolver: {
-//     //   require: fn => require(`${__dirname}/contracts/${fn}`)
-//     // },
-//     // resolver: require,
-//     resolver: {
-//       resolve: fn => require(`${__dirname}/${fn}.js`),
-//       require: fn => require(`${__dirname}/${fn}.js`)
-//     },
-//     solc // this might not be right
-//   }, callback2)
-
-  
-// }

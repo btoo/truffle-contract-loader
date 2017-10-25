0a40e9ae422e171b1a6e01207f4baa3634348dea
Brian Tu <feihcsim824@gmail.com>
2017-10-24 23:08:38 -0400

    optimize for legacy node v

--- index.js
+++ index.js
@@ -1,46 +1,44 @@
-const path = require('path')
-    , merge = require('./merge')
-    , truffleCompile = require('truffle-compile')
-    , loaderUtils = require('loader-utils')
+var path = require('path')
+  , merge = require('./merge')
+  , truffleCompile = require('truffle-compile')
+  , loaderUtils = require('loader-utils')
 
 module.exports = function(contents){
 
   this.cacheable && this.cacheable()
-  const options = loaderUtils.getOptions(this)
+  var options = loaderUtils.getOptions(this)
+    , file = this.resourcePath
 
   /**
   * to avoid having to save contract artifacts into their
   * own js fileswhenever a .sol dependency tree exists,
   * just reduce each tree down to its own single, concatenated file
   */
-  const file = this.resourcePath
-  const mergedSolidityFile = merge({
+  var mergedSolidityFile = merge({
     ...options,
     file
   })
 
-  const contractFileName = path.basename(this.resourcePath)
-  const contractName = contractFileName.charAt(0).toUpperCase() + contractFileName.slice(1, contractFileName.length - 4)
+  var contractFileName = path.basename(this.resourcePath)
+    , contractName = contractFileName.charAt(0).toUpperCase() + contractFileName.slice(1, contractFileName.length - 4)
+    , compilationFinished = this.async()
 
-  const compilationFinished = this.async()
-  const truffleCompileOptions = options
-
-  truffleCompile([mergedSolidityFile], truffleCompileOptions, (err, artifact) => {
+  truffleCompile([mergedSolidityFile], options, function(err, artifact){
 
     if(err) return compilationFinished(err)
-    const artifactString = `\`${
+    var artifactString = `\`${
       JSON.stringify(artifact)
         .replace(/(\\r\\n|\\n|\\r)/gm,'') // flatten
         .replace(/\\/gi, '\\\\') // escape
     }\``
 
     compilationFinished(err, `
-      const truffleContract = require('truffle-contract')
-      const contractName = '${contractName}'
+      var truffleContract = require('truffle-contract')
+      var contractName = '${contractName}'
 
       if(!window[contractName]){
-        const artifacts = JSON.parse(${artifactString})
-        const artifact = artifacts['${contractName}']
+        var artifacts = JSON.parse(${artifactString})
+        var artifact = artifacts['${contractName}']
         window[contractName] = truffleContract(artifact)
       }
 
